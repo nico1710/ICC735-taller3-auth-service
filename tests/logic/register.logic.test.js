fe45becf6132498231f3356f2ddce4755fbbad3b
NICOLÁS HIDALGO <n.hidalgo02@ufromail.cl>
2023-05-31 15:32:28 -0400

    Mejoras en el test register.logic

--- tests/logic/register.logic.test.js
+++ tests/logic/register.logic.test.js
@@ -1,13 +1,42 @@
 import { expect, sinon } from "../chai.config.js";
-import {checkIfUserAlreadyExists,generateCodeToken,checkIfRUTisAllowed,register} from "../../src/logic/register.logic.js";
+import register from "../../src/logic/register.logic.js";
 import UserModel from "../../src/models/user.model.js";
 import { HTTPError } from "../../src/helpers/error.helper.js";
 import registerMessages from "../../src/messages/register.messages.js";
 
 describe("Logic: Register logic test", ()=>{
+      
+    let registerLogicStub;
+
+	beforeEach(() => {
+		registerLogicStub = sinon.stub(UserModel, 'findOne');
+    });
+
+	afterEach(() => {
+		registerLogicStub.restore();
+	});
+     
 
       it("[ERROR] User is alredy register in website", async ()=>{
-           
+           const user ={
+              name: "Nicolas",
+              rut: "19972287-5",
+              email: "nicolas@gmail.com",
+              password: "P@ssword123"
+           };
+        registerLogicStub.resolves({});
+
+        const httpError ={
+            name: registerMessages.alreadyExists.name,
+            msg: registerMessages.alreadyExists.messages,
+            code: 400
+        }
+
+        try {
+			await register(user);
+		} catch (error) {
+			expect(error).to.equal(httpError);
+		}
       });
 
       it("[ERROR] Rut is allowed", async ()=>{
